8f2bf7c1a83b359c1962887c7bf76be8bb3f06a7
Myles Wirth <regime_skylark.0y@icloud.com>
2026-01-08 11:14:05 -0500

    Removed the odd lazy lock stuff

--- src/utils/nlp/parts_of_speech.rs
+++ src/utils/nlp/parts_of_speech.rs
@@ -1,137 +1,121 @@
-use std::{collections::HashSet, sync::LazyLock};
+use std::collections::HashSet;
 
 use tracing::trace;
 
 /// Subject pronouns, object pronouns, possessive adjectives, etc.
-static PRONOUNS: LazyLock<HashSet<&'static str>> = LazyLock::new(|| {
-	[
-		"i",
-		"you",
-		"he",
-		"she",
-		"it",
-		"we",
-		"they",
-		"me",
-		"you",
-		"him",
-		"her",
-		"it",
-		"us",
-		"them",
-		"my",
-		"your",
-		"his",
-		"her",
-		"its",
-		"our",
-		"their",
-		"mine",
-		"yours",
-		"his",
-		"hers",
-		"its",
-		"ours",
-		"theirs",
-		"myself",
-		"yourself",
-		"himself",
-		"herself",
-		"itself",
-		"ourselves",
-		"themselves",
-		"yourselves",
-		"this",
-		"that",
-		"these",
-		"those",
-		"who",
-		"whom",
-		"which",
-		"what",
-	]
-	.into_iter()
-	.collect()
-});
+const PRONOUNS: &[&str] = &[
+	"i",
+	"you",
+	"he",
+	"she",
+	"it",
+	"we",
+	"they",
+	"me",
+	"you",
+	"him",
+	"her",
+	"it",
+	"us",
+	"them",
+	"my",
+	"your",
+	"his",
+	"her",
+	"its",
+	"our",
+	"their",
+	"mine",
+	"yours",
+	"his",
+	"hers",
+	"its",
+	"ours",
+	"theirs",
+	"myself",
+	"yourself",
+	"himself",
+	"herself",
+	"itself",
+	"ourselves",
+	"themselves",
+	"yourselves",
+	"this",
+	"that",
+	"these",
+	"those",
+	"who",
+	"whom",
+	"which",
+	"what",
+];
 
 /// Common prepositions
-static PREPOSITIONS: LazyLock<HashSet<&'static str>> = LazyLock::new(|| {
-	[
-		"about",
-		"across",
-		"after",
-		"against",
-		"along",
-		"among",
-		"around",
-		"as",
-		"at",
-		"before",
-		"behind",
-		"beneath",
-		"beside",
-		"between",
-		"beyond",
-		"by",
-		"despite",
-		"during",
-		"except",
-		"for",
-		"from",
-		"in",
-		"inside",
-		"into",
-		"near",
-		"of",
-		"on",
-		"onto",
-		"out",
-		"outside",
-		"over",
-		"since",
-		"than",
-		"through",
-		"throughout",
-		"to",
-		"toward",
-		"under",
-		"until",
-		"upon",
-		"via",
-		"with",
-		"within",
-		"without",
-	]
-	.into_iter()
-	.collect()
-});
+const PREPOSITIONS: &[&str] = &[
+	"about",
+	"across",
+	"after",
+	"against",
+	"along",
+	"among",
+	"around",
+	"as",
+	"at",
+	"before",
+	"behind",
+	"beneath",
+	"beside",
+	"between",
+	"beyond",
+	"by",
+	"despite",
+	"during",
+	"except",
+	"for",
+	"from",
+	"in",
+	"inside",
+	"into",
+	"near",
+	"of",
+	"on",
+	"onto",
+	"out",
+	"outside",
+	"over",
+	"since",
+	"than",
+	"through",
+	"throughout",
+	"to",
+	"toward",
+	"under",
+	"until",
+	"upon",
+	"via",
+	"with",
+	"within",
+	"without",
+];
 
 /// Coordinating conjunctions
-static CONJUNCTIONS: LazyLock<HashSet<&'static str>> =
-	LazyLock::new(|| ["for", "and", "nor", "but", "or", "yet", "so"].into_iter().collect());
+const CONJUNCTIONS: &[&str] = &["for", "and", "nor", "but", "or", "yet", "so"];
 
 /// Articles
-static ARTICLES: LazyLock<HashSet<&'static str>> =
-	LazyLock::new(|| ["a", "an", "the"].into_iter().collect());
+const ARTICLES: &[&str] = &["a", "an", "the"];
 
 /// Words that determine quantity
-static PREDETERMINERS: LazyLock<HashSet<&'static str>> =
-	LazyLock::new(|| ["all", "both"].into_iter().collect());
+const PREDETERMINERS: &[&str] = &["all", "both"];
 
 /// Exceptions for predeterminers based on previous word
-static PREDETERMINERS_EXCEPTIONS_PREVIOUS_WORDS: LazyLock<HashSet<&'static str>> =
-	LazyLock::new(|| ["calling"].into_iter().collect());
+const PREDETERMINERS_EXCEPTIONS_PREVIOUS_WORDS: &[&str] = &["calling"];
 
 /// Other common words to filter out
-static OTHERS: LazyLock<HashSet<&'static str>> = LazyLock::new(|| {
-	[
-		"is", "are", "was", "were", "if", "will", "would", "be", "being", "one", "have", "has", "had",
-		"can", "more", "then", "do", "don't", "first", "even", "there", "only", "also", "such", "each",
-		"because", "however", "very", "must", "due",
-	]
-	.into_iter()
-	.collect()
-});
+const OTHERS: &[&str] = &[
+	"is", "are", "was", "were", "if", "will", "would", "be", "being", "one", "have", "has", "had",
+	"can", "more", "then", "do", "don't", "first", "even", "there", "only", "also", "such", "each",
+	"because", "however", "very", "must", "due",
+];
 
 /// Filter out words that are pronouns, prepositions, conjunctions, articles or
 /// some others.
@@ -140,26 +124,23 @@ static OTHERS: LazyLock<HashSet<&'static str>> = LazyLock::new(|| {
 /// words.
 pub fn filter_parts_of_speech(words: &[&str]) -> Vec<String> {
 	trace!("Filtering parts of speech from: {:?}", words);
-
 	let filtered = words
 		.iter()
 		.enumerate()
 		.filter(|(idx, word)| {
 			let previous_word = if *idx > 0 { Some(&words[idx - 1]) } else { None };
-
 			// Skip if it's a pronoun, preposition, etc.
-			!(PRONOUNS.contains(*word)
-				|| PREPOSITIONS.contains(*word)
-				|| CONJUNCTIONS.contains(*word)
-				|| ARTICLES.contains(*word)
-				|| (PREDETERMINERS.contains(*word)
+			!(PRONOUNS.contains(word)
+				|| PREPOSITIONS.contains(word)
+				|| CONJUNCTIONS.contains(word)
+				|| ARTICLES.contains(word)
+				|| (PREDETERMINERS.contains(word)
 					&& !(previous_word.is_some()
 						&& PREDETERMINERS_EXCEPTIONS_PREVIOUS_WORDS.contains(previous_word.unwrap())))
-				|| OTHERS.contains(*word))
+				|| OTHERS.contains(word))
 		})
 		.map(|(_, word)| (*word).to_owned())
 		.collect();
-
 	trace!("Filtered result: {:?}", filtered);
 	filtered
 }
